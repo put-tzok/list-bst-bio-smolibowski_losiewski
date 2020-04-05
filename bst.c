41aa43ef0bcb78a4f4cb32cea319ed2a4b2af3bc
Smolibek <61935541+Smolibek@users.noreply.github.com>
2020-04-05 22:01:18 +0200

    Update bst.c

--- bst.c
+++ bst.c
@@ -5,8 +5,7 @@
 #include <signal.h>
 #include <time.h>
 
-unsigned int ns[] = { 1000, 2000, 3000, 4000, 5000, 6000, 7000/* TODO: fill values which will be used as lists' sizes */ };
-
+unsigned int ns[] = {5000, 10000, 20000, 30000,40000 ,50000, 65000};/* TODO: fill values which will be used as lists' sizes */
 
 
 struct node {
